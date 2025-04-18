1a4c6b99bc05768b37406010e1f73e67c9aaa4de
ksr03 <K_Haruka03@outlook.jp>
2025-04-18 14:34:09 +0900

    画面幅で画像を切り替える

--- src/components/sections/MainVisual/MainVisual.tsx
+++ src/components/sections/MainVisual/MainVisual.tsx
@@ -2,16 +2,30 @@ import { useEffect, useState } from "react";
 import styles from "./MainVisual.module.css";
 
 export const MainVisual = () => {
-  const images = ["images/img1.png", "images/img2.png", "images/img3.png"];
+  const basePath = 'images/main_visual/';
+  const Images = [
+    {
+      pc: `${basePath}pc_1.png`,
+      sp: `${basePath}sp_1.png`,
+    },
+    {
+      pc: `${basePath}pc_2.png`,
+      sp: `${basePath}sp_2.png`,
+    },
+    {
+      pc: `${basePath}pc_3.png`,
+      sp: `${basePath}sp_3.png`,
+    },
+  ]
 
   const [index, setIndex] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setIndex((prevIndex) => (prevIndex + 1) % images.length);
+      setIndex((prevIndex) => (prevIndex + 1) % Images.length);
     }, 5000);
     return () => clearInterval(interval);
-  }, [images.length, index]);
+  }, [Images.length, index]);
 
   return (
     <section id="main-visual">
@@ -30,17 +44,20 @@ export const MainVisual = () => {
             className={styles.img_wrapper}
             style={{
               transform: `translateX(-${index * 100}vw)`,
-              width: `${images.length * 100}vw`,
+              width: `${Images.length * 100}vw`,
             }}
           >
-            {images.map((image, i) => (
-              <img key={i} src={image} alt="VRCの画像" className={styles.img} />
+            {Images.map((image, i) => (
+              <picture key={i}>
+                <source media="(max-width: 767px)" srcSet={image.sp} className={styles.img} />
+                <img src={image.pc} alt="VRCの画像" className={styles.img} />
+              </picture>
             ))}
           </div>
         </div>
         {/* スライドショー下のボタン */}
         <div className={styles.button_wrapper}>
-          {images.map((_, i) => (
+          {Images.map((_, i) => (
             <button
               key={i}
               className={`${styles.button} ${i === index ? styles.active : ""}`}
